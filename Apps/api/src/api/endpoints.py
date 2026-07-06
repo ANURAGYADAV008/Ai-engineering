97cd6751cb044e51d4b7fbaeca15cf81f7c379f5
Anurag Yadav <jaysinghanurag321@gmail.com>
2026-07-06 16:07:15 +0530

    feat: initialize API project structure and add RAG pipeline notebook

---
+++ Apps/api/src/api/endpoints.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Request
+from api.api.models import RAGRequest, RAGResponse, RAGUsedContext
+from api.agents.retrieval_generation import rag_pipeline_wrapper
+from qdrant_client import QdrantClient
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+rag_router = APIRouter()
+
+@rag_router.post("/")
+def chat(request: Request, payload: RAGRequest) -> RAGResponse:
+    result = rag_pipeline_wrapper(payload.query)
+    return RAGResponse(
+        answer=result['answer'],
+        used_context=[RAGUsedContext(**item) for item in result['used_context']]
+    )
+
+api_router = APIRouter()
+api_router.include_router(rag_router, prefix="/rag", tags=["rag"])
